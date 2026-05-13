c9a03e2e3a0e0bc7ba412a81fd6fe3ea3766cd11
devanandan s <devan@globaleducationsolutions.org>
2026-05-13 15:34:39 +0530

    d

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
-import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 import Projects from "./components/Projects";
@@ -19,7 +18,6 @@ export default function Home() {
         <Skills />
         <Contact />
       </main>
-      <Footer />
     </>
   );
 }
